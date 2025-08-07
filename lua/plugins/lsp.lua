2f0774ffd62b15177e946d3aaf85c2b70f57d9bf
Elliot Chan <elliot.chan.mk@gmail.com>
2025-08-08 02:30:07 +0800

    first commit

---
+++ lua/plugins/lsp.lua
@@ -0,0 +1,22 @@
+local lspconfig = require("lspconfig")
+local configs = require("lspconfig.configs")
+
+return {
+  "neovim/nvim-lspconfig",
+  config = function()
+    if not configs.cangjie_lsp then
+      configs.cangjie_lsp = {
+        default_config = {
+          cmd = { "/home/elliot/tools/SDK/cangjie/tools/bin/LSPServer", "--log-path", "/home/elliot/" },
+          root_dir = lspconfig.util.root_pattern(".git"),
+          filetypes = { "Cangjie" },
+          on_attach = function(client, bufnr)
+            print("Cangjie LSP started")
+          end,
+          capabilities = vim.lsp.protocol.make_client_capabilities(),
+        },
+      }
+    end
+    lspconfig.cangjie_lsp.setup({})
+  end,
+}
